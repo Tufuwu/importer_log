58175c09bd953db66a82b7647a700b13643f63b0
wu <wu@.com>
2026-01-29 02:06:05 +0800

    Logs-damonoehlman/wildcard

--- cypress/support/index.js
+++
@@ -1,8 +0,0 @@
-beforeEach(function() {
-    /* Clear the session storage, as it is used to restore field data;
-     * and we don't want a test to load data from the previous test. */
-    cy.window().then((win) => {
-        win.sessionStorage.clear()
-    })
-    cy.visit('./index.html');
-});
