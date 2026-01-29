ca013890f7899bca448758f07a64979070608615
wu <wu@.com>
2026-01-29 12:32:55 +0800

    Logs-cuducos/webassets-elm

--- setup.py
+++ setup.py
@@ -1,34 +1,32 @@
+from pathlib import Path
 from setuptools import setup
 
+
 setup(
-    name='neat-python',
-    version='1.0.0',
-    author='Alan Mcintyre, Cesar Gomes Miguel, Carolina Feher da Silva, Marcio Lobo Netto',
-    author_email='alan@codereclaimers.com',
-    maintainer='Alan McIntyre',
-    maintainer_email='alan@codereclaimers.com',
-    url='https://github.com/CodeReclaimers/neat-python',
-    license="BSD",
-    description='A NEAT (NeuroEvolution of Augmenting Topologies) implementation',
-    long_description='Python implementation of NEAT (NeuroEvolution of Augmenting Topologies), a method ' +
-                     'developed by Kenneth O. Stanley for evolving arbitrary neural networks. ' +
-                     'Version 1.0.0 includes full innovation number tracking as described in the original NEAT paper.',
-    long_description_content_type= 'text/x-rst',
-    packages=['neat', 'neat/iznn', 'neat/nn', 'neat/ctrnn'],
+    author="Eduardo Cuducos",
+    author_email="cuducos@gmail.com",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Scientific/Engineering'
-    ]
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Flask",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="Elm filter for webassets",
+    install_requires=["webassets"],
+    keywords=["elm", "webassets", "assets", "django", "flask"],
+    license="MIT",
+    long_description=Path("README.rst").read_text(),
+    name="webassets-elm",
+    py_modules=["webassets_elm"],
+    python_requires=">=3.5",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    url="https://github.com/cuducos/webassets-elm",
+    version="0.2.1",
+    zip_safe=False,
 )
