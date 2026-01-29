f8e6a11455da8d15d7569bf724b1e2c319b99727
wu <wu@.com>
2026-01-29 12:46:50 +0800

    Logs-earwig/mwparserfromhell

--- docs/index.rst
+++ docs/index.rst
@@ -1,19 +1,55 @@
-.. complexity documentation master file, created by
-   sphinx-quickstart on Tue Jul  9 22:26:36 2013.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+MWParserFromHell v\ |version| Documentation
+===========================================
 
-Welcome to drf-tus's documentation!
-=================================================================
+:mod:`mwparserfromhell` (the *MediaWiki Parser from Hell*) is a Python package
+that provides an easy-to-use and outrageously powerful parser for MediaWiki_
+wikicode. It supports Python 3.5+.
 
-Contents:
+Developed by Earwig_ with contributions from `Σ`_, Legoktm_, and others.
+Development occurs on GitHub_.
+
+.. _MediaWiki:            https://www.mediawiki.org
+.. _Earwig:               https://en.wikipedia.org/wiki/User:The_Earwig
+.. _Σ:                    https://en.wikipedia.org/wiki/User:%CE%A3
+.. _Legoktm:              https://en.wikipedia.org/wiki/User:Legoktm
+.. _GitHub:               https://github.com/earwig/mwparserfromhell
+
+Installation
+------------
+
+The easiest way to install the parser is through the `Python Package Index`_;
+you can install the latest release with ``pip install mwparserfromhell``
+(`get pip`_). Make sure your pip is up-to-date first, especially on Windows.
+
+Alternatively, get the latest development version::
+
+    git clone https://github.com/earwig/mwparserfromhell.git
+    cd mwparserfromhell
+    python setup.py install
+
+The comprehensive unit testing suite requires `pytest`_ (``pip install pytest``)
+and can be run with ``python -m pytest``.
+
+.. _Python Package Index:   https://pypi.org/
+.. _get pip:                https://pypi.org/project/pip/
+.. _pytest:                 https://docs.pytest.org/
+
+Contents
+--------
 
 .. toctree::
    :maxdepth: 2
 
-   readme
-   installation
    usage
-   contributing
-   authors
-   history
+   limitations
+   integration
+   changelog
+   API Reference <api/modules>
+
+
+Indices and tables
+------------------
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- setup.py
+++ setup.py
@@ -1,96 +1,115 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
+#
+# Copyright (C) 2012-2021 Ben Kurtovic <ben.kurtovic@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from glob import glob
 import os
-import re
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import find_packages, setup, Extension
+from setuptools.command.build_ext import build_ext
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+
+from mwparserfromhell import __version__
+
+with open("README.rst") as fp:
+    long_docs = fp.read()
+
+use_extension = True
+fallback = True
+
+# Allow env var WITHOUT_EXTENSION and args --with[out]-extension:
+
+env_var = os.environ.get("WITHOUT_EXTENSION")
+if "--without-extension" in sys.argv:
+    use_extension = False
+elif "--with-extension" in sys.argv:
+    fallback = False
+elif env_var is not None:
+    if env_var == "1":
+        use_extension = False
+    elif env_var == "0":
+        fallback = False
+
+# Remove the command line argument as it isn't understood by setuptools:
+
+sys.argv = [
+    arg for arg in sys.argv if arg not in ("--without-extension", "--with-extension")
+]
 
 
-def get_version(*file_paths):
-    """Retrieves the version from rest_framework_tus/__init__.py"""
-    filename = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = open(filename).read()
-    version_match = re.search(
-        r"^__version__ = ['\"]([^'\"]*)['\"]",
-        version_file,
-        re.M,
-    )
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
-version = get_version("rest_framework_tus", "__init__.py")
-
-
-if sys.argv[-1] == "publish":
+def build_ext_patched(self):
     try:
-        import wheel
+        build_ext_original(self)
+    except Exception as exc:
+        print("error: " + str(exc))
+        print("Falling back to pure Python mode.")
+        del self.extensions[:]
 
-        print("Wheel version: ", wheel.__version__)
-    except ImportError:
-        print('Wheel library missing. Please run "pip install wheel"')
-        sys.exit()
-    os.system("python setup.py sdist upload")
-    os.system("python setup.py bdist_wheel upload")
-    sys.exit()
 
-if sys.argv[-1] == "tag":
-    print("Tagging the version on git:")
-    os.system(f"git tag -a {version} -m 'version {version}'")
-    os.system("git push --tags")
-    sys.exit()
+if fallback:
+    build_ext.run, build_ext_original = build_ext_patched, build_ext.run
 
-readme = open("README.rst").read()
-history = open("HISTORY.rst").read().replace(".. :changelog:", "")
+# Project-specific part begins here:
+
+tokenizer = Extension(
+    "mwparserfromhell.parser._tokenizer",
+    sources=sorted(glob("src/mwparserfromhell/parser/ctokenizer/*.c")),
+    depends=sorted(glob("src/mwparserfromhell/parser/ctokenizer/*.h")),
+)
 
 setup(
-    name="drf-tus",
-    version=version,
-    description="A Tus (tus.io) library for Django Rest Framework",
-    long_description=f"{readme}\n\n{history}",
-    author="Dirk Moors",
-    author_email="dirkmoors@gmail.com",
-    url="https://github.com/dirkmoors/drf-tus",
-    packages=[
-        "rest_framework_tus",
-    ],
-    include_package_data=True,
-    install_requires=[
-        "python-dateutil>=2.8.2",
-        "Django>=3.2,<=4.2",
-        "djangorestframework>=3.14.0",
-        "jsonfield>=2.0.0",
-        "django-fsm==2.8.1",
-        "six>=1.11.0",
-    ],
-    license="MIT",
-    zip_safe=False,
-    keywords=[
-        "drf-tus",
-        "tus",
-        "django",
-        "rest",
-        "framework",
-        "django-rest-framework",
-    ],
+    name="mwparserfromhell",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    ext_modules=[tokenizer] if use_extension else [],
+    setup_requires=["pytest-runner"]
+    if "test" in sys.argv or "pytest" in sys.argv
+    else [],
+    tests_require=["pytest"],
+    version=__version__,
+    python_requires=">= 3.6",
+    author="Ben Kurtovic",
+    author_email="ben.kurtovic@gmail.com",
+    url="https://github.com/earwig/mwparserfromhell",
+    description="MWParserFromHell is a parser for MediaWiki wikicode.",
+    long_description=long_docs,
+    download_url="https://github.com/earwig/mwparserfromhell/tarball/v{}".format(
+        __version__
+    ),
+    keywords="earwig mwparserfromhell wikipedia wiki mediawiki wikicode template parsing",
+    license="MIT License",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Framework :: Django",
-        "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4.0",
-        "Framework :: Django :: 4.1",
-        "Framework :: Django :: 4.2",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Markup",
     ],
 )
