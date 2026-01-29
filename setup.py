11fb3f9a3e26630dfde1faa51cda05ab223c1eeb
wu <wu@.com>
2026-01-29 14:25:44 +0800

    Logs-xyproto/setconf

--- setup.py
+++ setup.py
@@ -1,48 +1,27 @@
+# -*- coding: utf-8 -*-
 import os
+import sys
+
 from setuptools import setup
 
-this = os.path.dirname(os.path.realpath(__file__))
-
-
-def read(name):
-    with open(os.path.join(this, name)) as f:
-        return f.read()
-
-
-setup(
-    name="sqlalchemy_mptt",
-    version="0.2.5",
-    url="http://github.com/uralbash/sqlalchemy_mptt/",
-    author="Svintsov Dmitry",
-    author_email="sacrud@uralbash.ru",
-    packages=["sqlalchemy_mptt"],
-    include_package_data=True,
-    zip_safe=False,
-    test_suite="nose.collector",
-    license="MIT",
-    description="SQLAlchemy MPTT mixins (Nested Sets)",
-    long_description=read("README.rst") + "\n" + read("CHANGES.rst"),
-    install_requires=read("requirements.txt"),
-    tests_require=read("requirements.txt") + read("requirements-test.txt"),
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Framework :: Pyramid",
-        "Framework :: Flask",
-        "Topic :: Internet",
-        "Topic :: Database",
-        "License :: OSI Approved :: MIT License",
-    ],
-)
+setup(name="setconf",
+      version="0.7.7",
+      description="Change configuration settings in text files",
+      url="https://setconf.roboticoverlords.org/",
+      author="Alexander F. Rødseth",
+      author_email="xyproto@archlinux.org",
+      license="GPLv2",
+      py_modules=["setconf"],
+      entry_points={
+          "console_scripts": [
+              "setconf = setconf:main",
+          ]
+      },
+      classifiers=[
+          "Environment :: Console",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Programming Language :: Python",
+          "Topic :: System :: Shells",
+          "Topic :: Utilities",
+      ]
+      )
