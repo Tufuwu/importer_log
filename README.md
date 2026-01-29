3d43c947dba309595bb667bfafa6b1b709369228
wu <wu@.com>
2026-01-29 14:05:41 +0800

    Logs-scVENUS/PeekabooAV

--- README.md
+++ README.md
@@ -1,75 +1,106 @@
-[![Build Status](https://travis-ci.org/roverdotcom/django-inlinecss.svg?branch=master)](https://travis-ci.org/roverdotcom/django-inlinecss)
+# PeekabooAV #
 
-## About
+[![Testsuite](https://github.com/scVENUS/PeekabooAV/actions/workflows/testsuite.yml/badge.svg)](https://github.com/scVENUS/PeekabooAV/actions/workflows/testsuite.yml)
 
-Inlining CSS is necessary for email generation and sending
-but is currently a surprisingly large hassle.
+**Peekaboo Extended Email Attachment Behavior Observation Owl**
 
-This library aims to make it a breeze in the Django
-template language.
+* PeekabooAV is an Anti Virus software
+* It gets email attachments from AMaViSd, checks them, uses Cuckoo for behavioral checks, and evaluates and rates fully automatic
+* PeekabooAV is written in Python, multi-threaded, scalable, has a very powerful ruleset, and is easy to extend and personalize
+* It is able to detect: malware by its behavior, exploitation of zero days, and targeted attacks
 
-## Usage
+* We develop and work in Germany
+* PeekabooAV is entirely open source
+* can run 100% local and does not require any external service
+* any file with any application can be scanned
 
-#### Step 1: Dependencies
+If you run mail on-site, distrust security vendors and need a high level of security and secrecy
+PeekabooAV is for you.
 
-- BeautifulSoup
-- cssutils
-- Python 3.8+
-- Django 3.2+
+For news and announcements follow us on twitter [@peekabooAV](https://twitter.com/peekabooav).
 
 
-#### Step 2: Install django_inlinecss
 
-Add ```django_inlinecss``` to your ```settings.py```:
+## Getting Started ##
 
-```python
-INSTALLED_APPS = (
-        'django.contrib.auth',
-        'django.contrib.webdesign',
-        'django.contrib.contenttypes',
-        '...',
-        '...',
-        '...',
-        'django_inlinecss')
+### Prerequisites ####
+
+* [Python](https://www.python.org/downloads/) (3.6+ required)
+* [Cuckoo 2.0](https://github.com/cuckoosandbox/cuckoo)
+* [AMaViSd 2.11.0](https://www.ijs.si/software/amavisd/)
+* Installer is available: [PeekabooAV-Installer](https://github.com/scVENUS/PeekabooAV-Installer)
+
+
+### Installation ###
+
+Install from PyPI into a new virtual environment:
+```shell
+virtualenv --python=python3 /path/to/venv
+/path/to/venv/bin/pip install peekabooav
 ```
 
-#### Step 3: Use the templatetag
-
-1. Place your CSS file somewhere staticfiles can find it
-2. Create your template:
-
-```html
-{% load inlinecss %}
-{% inlinecss "css/extra-padding.css" %}
-    <html>
-        <body>
-            <div class='lots-o-padding'>
-                Something in need of styling.
-            </div>
-        </body>
-    </html>
-{% endinlinecss %}
+Or use this repository:
+```shell
+git clone https://github.com/scVENUS/PeekabooAV.git
+virtualenv --python=python3 /path/to/venv
+/path/to/venv/bin/pip install .
 ```
 
-#### Step 4: Prepare to be Wowed
+This will pull in all required packages and install them into the virtualenv.
 
-```html
-<html>
-    <body>
-        <div style="padding-left: 10px; padding-right: 10px; padding-top: 10px;" class="lots-o-padding">
-            Something in need of styling.
-        </div>
-    </body>
-</html>
+### Configuration ###
+Take a look at ``peekaboo.conf.sample`` and ``ruleset.conf.sample``.
+
+
+## Running the tests ##
+
+Runs the unit tests
+```shell
+/path/to/venv/bin/python tests/test.py
 ```
 
-## Acknowledgements
 
-Thanks to Tanner Netterville for his efforts on [Pynliner](https://github.com/rennat/pynliner).
+## Usage ##
 
-Thanks to Thomas Yip for his unit tests on the `soupselect` module. These tests
-helped on getting the core CSS2 selectors to work.
+Now, you can run PeekabooAV with
+```shell
+/path/to/venv/bin/peekaboo -c /path/to/your/peekaboo.conf
+```
 
-## License
+**Note:** If you put your PeekabooAV configuration file at
+``/opt/peekaboo/etc/peekaboo.conf`` you can omit the ``-c`` option.
+Also, for detailed command line options run
+```shell
+peekaboo --help
+```
 
-MIT license. See LICENSE.md for more detail.
+### Development Quickstart ###
+
+Just install Peekaboo using pip like above but in editable/development mode:
+
+```shell
+/path/to/venv/bin/pip install -e .
+```
+
+Now you can run it as before but changes to the source code will take effect
+without reinstallation.
+See the [development documentation](docs/source/development.rst) for details.
+
+## Contributing ##
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+
+## Versioning ##
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available,
+see the [tags on this repository](https://github.com/scVENUS/PeekabooAV/releases).
+
+## Past and present Developers and Contributors ##
+
+* **Felix Bauer** - *Security Analyst and Project Leader* - [@Jack28](https://github.com/Jack28)
+* **Michael Weiser** - *Developer* - [@michaelweiser](https://github.com/michaelweiser)
+* **Sebastian Deiss** - *Former Technical Lead* - [@SebastianDeiss](https://github.com/SebastianDeiss)
+
+## License ##
+
+This project is licensed under the GPL 3 license - see the [LICENSE.txt](LICENSE.txt) file for details.
--- setup.py
+++ setup.py
@@ -1,137 +1,131 @@
-import os
-import sys
-from shutil import rmtree
+#!/usr/bin/env python
 
-from setuptools import Command
-from setuptools import find_packages
-from setuptools import setup
-
-# Package meta-data.
-NAME = 'django-inlinecss'
-SRC_DIR = 'django_inlinecss'
-DESCRIPTION = 'A Django app useful for inlining CSS (primarily for e-mails)'
-URL = 'https://github.com/roverdotcom/django-inlinecss'
-EMAIL = 'philip@rover.com'
-AUTHOR = 'Philip Kimmey'
-REQUIRES_PYTHON = '>=3.8'
-VERSION = None
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    'Django>=3.2',
-    'pynliner',
-    'future>=0.16.0',
-]
-
-# What packages are required only for tests?
-TESTS = [
-    'mock==5.1.0',
-    'pytest==8.0.1',
-    'pytest-django==4.8.0',
-]
-
-# What packages are optional?
-EXTRAS = {
-    'flake8': [
-        'flake8==7.0.0',
-        'flake8-isort==6.1.1',
-        'isort==5.13.2',
-        'testfixtures==8.0.0',
-    ],
-    "tests": TESTS,
-}
-
-here = os.path.abspath(os.path.dirname(__file__))
+###############################################################################
+#                                                                             #
+# Peekaboo Extended Email Attachment Behavior Observation Owl                 #
+#                                                                             #
+# setup.py                                                                    #
+###############################################################################
+#                                                                             #
+# Copyright (C) 2016-2022 science + computing ag                              #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or (at       #
+# your option) any later version.                                             #
+#                                                                             #
+# This program is distributed in the hope that it will be useful, but         #
+# WITHOUT ANY WARRANTY; without even the implied warranty of                  #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU           #
+# General Public License for more details.                                    #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
 
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+from codecs import open
+from os import path
+from sys import path as pythonpath
+from setuptools import setup, find_packages
 
-try:
-    with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-        long_description = "\n" + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
+# Add peekaboo to PYTHONPATH
+pythonpath.append(path.dirname(path.dirname(path.abspath(__file__))))
+from peekaboo import __version__, __author__, __license__, __description__
 
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    with open(os.path.join(here, SRC_DIR, "__version__.py")) as f:
-        exec(f.read(), about)
-else:
-    about["__version__"] = VERSION
+here = path.abspath(path.dirname(__file__))
 
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
 
-    description = "Build and publish the package."
-    user_options = []
+install_requires = []
+for req in all_reqs:
+    # for git repo references, extract the egg/package name from the end and
+    # prepend it with @ as per pip Github issue 3939
+    if 'git+' in req:
+        egg = req.split('=')[-1]
+        req = "%s @ %s" % (egg, req)
 
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print(f"\033[1m{s}\033[0m")
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status("Removing previous builds…")
-            rmtree(os.path.join(here, "dist"))
-        except OSError:
-            pass
-
-        self.status("Building Source and Wheel (universal) distribution…")
-        os.system(f"{sys.executable} setup.py sdist bdist_wheel --universal")
-
-        self.status("Uploading the package to PyPI via Twine…")
-        os.system("twine upload dist/*")
-
-        self.status("Pushing git tags…")
-        os.system("git tag v{}".format(about["__version__"]))
-        os.system("git push --tags")
-
-        sys.exit()
-
+    install_requires.append(req.strip())
 
 setup(
-    name=NAME,
-    version=about["__version__"],
-    description=DESCRIPTION,
+    name='PeekabooAV',
+    version=__version__,
+    description=__description__,
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    author=AUTHOR,
-    author_email=EMAIL,
-    license="MIT",
-    url=URL,
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    keywords=["html", "css", "inline", "style", "email"],
+    url='https://github.com/scVENUS/PeekabooAV.git',
+    download_url='https://github.com/scVENUS/PeekabooAV/archive/master.zip',
+    license=__license__,
     classifiers=[
-        "Environment :: Other Environment",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Topic :: Communications :: Email",
-        "Topic :: Text Processing :: Markup :: HTML",
+      'Development Status :: 4 - Beta',
+      'Operating System :: POSIX',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Natural Language :: English',
+      'Natural Language :: German',
+      'Topic :: Communications :: Email :: Filters',
     ],
-    install_requires=REQUIRED,
-    tests_require=TESTS,
-    extras_require=EXTRAS,
-    # $ setup.py upload support.
-    cmdclass={
-        "upload": UploadCommand,
-    },
+    keywords='Cuckoo, Amavis',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    # package_files augments MANIFEST.in in what is packaged into a
+    # distribution. Files to add must be inside a package. Thus files in the
+    # root of our source directory cannot be packaged with this. Files inside
+    # packages will stay there, totally obscured to the user. Meant for default
+    # configuration or other package-internal data.
+    #  package_files=[...],
+    #
+    # data_files is another way to augment what is installed from the
+    # distribution. Allows paths outside packages for both sources and targets.
+    # Absolute paths are strongly discouraged because they exhibit confusing if
+    # not downright broken behaviour. Relative paths are added to the
+    # distribution and then installed into
+    # site-packages/<package>-<ver>.egg/<relative path> (setup.py) or <python
+    # prefix>/<relative path> (pip). The latter works well with venvs and
+    # distribution-provided pip (e.g. /usr/local/<relative path>).
+    # We go this route for providing sample files because using setup.py
+    # directly is discouraged anyway and "only" tucks the files away in the egg
+    # directory, providing the most consistent option.
+    data_files=[
+        ("share/doc/peekaboo", [
+            "README.md",
+            "CHANGELOG.md",
+            "peekaboo.conf.sample",
+            "ruleset.conf.sample",
+            "analyzers.conf.sample",
+        ]),
+        ("share/doc/peekaboo/systemd", [
+            "systemd/peekaboo.service",
+        ]),
+        ("share/doc/peekaboo/amavis", [
+            "amavis/10-ask_peekaboo",
+        ]),
+    ],
+    # overriding the whole install_data command allows for arbitrary
+    # installation mechanics but does not solve the problem of adding files to
+    # a binary distribution (e.g. wheel, which pip uses internally always) in
+    # such a way that they will later be put at the correct location. Thus they
+    # would go around pip entirely, be missing from any actual wheel
+    # distribution package, pollute the system directly and not be removed upon
+    # uninstall or upgrade.
+    #  cmdclass={
+    #      'install_data': OffsetDataInstall,
+    #  },
+    author=__author__,
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    author_email='felix.bauer@atos.net',
+    entry_points={
+        'console_scripts': [
+            'peekaboo = peekaboo.daemon:run',
+            'peekaboo-util = peekaboo.cli:main',
+        ],
+    }
 )
