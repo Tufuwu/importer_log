5bc855b0fe6338e5f2f9ac2970dd778d0d32bdf3
wu <wu@.com>
2026-01-29 13:37:24 +0800

    Logs-mgaitan/sphinxcontrib-mermaid

--- setup.py
+++ setup.py
@@ -1,52 +1,64 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# -*- mode: python -*-
-from setuptools import setup, find_packages, Extension
-import sys
-if sys.version_info[:2] < (2, 7) or (3, 0) <= sys.version_info[:2] < (3, 5):
-    raise RuntimeError("Python version 2.7 or >= 3.5 required.")
 
-# ---- Metadata ---- #
-VERSION = '1.3.7'
+import io
+from setuptools import setup, find_packages
 
-cls_txt = """
-Development Status :: 5 - Production/Stable
-Intended Audience :: Science/Research
-License :: OSI Approved :: GNU General Public License (GPL)
-Programming Language :: Python
-Topic :: Scientific/Engineering
-Operating System :: Unix
-Operating System :: POSIX :: Linux
-Operating System :: MacOS :: MacOS X
-Natural Language :: English
-"""
+readme = io.open('README.rst', encoding="utf-8").read()
+changes = io.open('CHANGELOG.rst', encoding="utf-8").read()
+version = '0.7.1'
 
-long_desc = """
-This is a very basic but very fast window discriminator for detecting and
-extracting spikes in a time series. It was developed for analyzing extracellular
-neural recordings, but also works with intracellular data and probably many
-other kinds of time series.
-"""
 
-#####
+def long_description():
+    """
+    return readme + changes, removing directive blocks that are only valid in the context
+    of sphinx doc"""
 
-from Cython.Distutils import build_ext
-_spikes = Extension('quickspikes.spikes', sources=['quickspikes/spikes.pyx'])
+    def remove_block(text, token, margin=0):
+        input_lines = text.splitlines()
+        for i, l in enumerate(input_lines):
+            if l.startswith(token):
+                break
+        start = i
+        end = input_lines.index("", start + margin)
+        return "\n".join(input_lines[:start] + input_lines[end:])
+
+    readme_ = remove_block(readme, ".. mermaid::", margin=2)
+    readme_ = remove_block(readme_, ".. autoclasstree::")
+    readme_ = remove_block(readme_, ".. autoclasstree::")
+    readme_ = remove_block(readme_, ".. versionchanged::")
+    return "{}\n\n{}".format(readme_, changes)
+
 
 setup(
-    name='quickspikes',
-    version=VERSION,
-    packages=find_packages(exclude=["*test*"]),
-    ext_modules=[_spikes],
-    cmdclass={'build_ext': build_ext},
-    scripts=[],
-    description="detect and extract spikes in time series data",
-    long_description=long_desc,
-    classifiers=[x for x in cls_txt.split("\n") if x],
-    install_requires=["numpy"],
-
-    author="Dan Meliza",
-    maintainer="Dan Meliza",
-    url='http://github.com/melizalab/quickspikes',
-    test_suite="tests"
+    name='sphinxcontrib-mermaid',
+    version=version,
+    url='https://github.com/mgaitan/sphinxcontrib-mermaid',
+    download_url='https://pypi.python.org/pypi/sphinxcontrib-mermaid',
+    license='BSD',
+    author=u'Martín Gaitán',
+    author_email='gaitan@gmail.com',
+    description='Mermaid diagrams in yours Sphinx powered docs',
+    long_description=long_description(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    namespace_packages=['sphinxcontrib'],
 )
