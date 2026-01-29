44ff4bcf032c432bfd7d5138d4b3ca8cf14cb608
wu <wu@.com>
2026-01-29 13:13:47 +0800

    Logs-jpmckinney/validictory

--- setup.py
+++ setup.py
@@ -1,55 +1,39 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import find_packages, setup
 
-here = os.path.abspath(os.path.dirname(__file__))
+from validictory import __version__
 
-def read_file(filename):
-    path = os.path.join(here, filename)
-    return open(path, encoding="utf-8").read() if os.path.exists(path) else ""
+with open('README.rst') as f:
+    long_description = f.read()
 
-README = read_file("README.rst")
-NEWS = read_file("NEWS.txt")
-
-version = "0.5.0"
-
-install_requires = [
-    "urllib3>=1.7.1",
-    "dnspython>=1.13.0",
-]
-
-extras_require = {
-    "test": [
-        "pytest",
-        "pytest-mock",
-        "pyOpenSSL>=0.14",
-    ]
-}
-
-setup(
-    name="python-etcd",
-    version=version,
-    description="A python client for etcd",
-    long_description=README + "\n\n" + NEWS,
-    long_description_content_type="text/x-rst",
-    classifiers=[
-        "Topic :: System :: Distributed Computing",
-        "Topic :: Software Development :: Libraries",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Database :: Front-Ends",
-    ],
-    keywords="etcd raft distributed log api client",
-    author="Jose Plana",
-    author_email="jplana@gmail.com",
-    url="http://github.com/jplana/python-etcd",
-    license="MIT",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-
-    # Modern testing: no nose, no test_suite
-    extras_require=extras_require,
+setup(name='validictory',
+      version=__version__,
+      description='general purpose python data validator',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
+      author='James Turk',
+      author_email='james.p.turk@gmail.com',
+      url='http://github.com/jamesturk/validictory',
+      license='MIT',
+      classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: MIT License",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
+                   'Programming Language :: Python :: 3.10',
+                   'Programming Language :: Python :: 3.11',
+                   'Programming Language :: Python :: 3.12',
+                   'Programming Language :: Python :: Implementation :: CPython',
+                   'Programming Language :: Python :: Implementation :: PyPy',
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   ],
+      packages=find_packages(exclude=['validictory.tests', 'validictory.tests.*']),
+      extras_require={
+          'test': [
+              'pytest',
+              'pytest-cov',
+          ]
+      }
 )
