379b2ed0ab1da46cd607fe1ccf1d0d5621ed3653
wu <wu@.com>
2026-01-29 09:31:10 +0800

    Logs-bendrucker/document-ready

--- README.md
+++ README.md
@@ -1,76 +1,37 @@
-# better-title-case
+# document-ready
 
-> Convert a string to title case based on the [Daring Fireball](https://daringfireball.net/2008/05/title_case) rules.
+> Document ready listener for modern browsers
 
-## Rules
 
-- If the string is all-caps, it will be corrected
-- The following words are not capitalized by default: a, an, and, at, but, by, for, in, nor, of, on, or, so, the, to, up, yet, v, v., vs, and vs.
-- Words with capital letters other than the first are assumed to be capitalized properly and are skipped
-- It also skips any word that looks like a file path, file name, or URL
-- The first and last word are always capitalized
-- Sub-strings (those that are within quotes or parens/braces) are capitalized according to the same rules
-
-## Installation
+## Install
 
 ```
-$ npm install --save better-title-case
+$ npm install --save document-ready
 ```
+
 
 ## Usage
 
 ```js
-import titleCase from 'better-title-case';
-console.log(titleCase('Nothing to Be Afraid of?'));
-// Nothing to Be Afraid Of?
+var ready = require('document-ready')
+
+ready(function () {
+  //=> DOM is ready
+})
 ```
 
-## Advanced
+## API
 
-You can configure `better-title-case` to add your own excluded words to the default list, or to prevent the use of the default list by passing a `config` object as the second parameter.
+#### `ready(callback)` -> `undefined`
 
-### excludedWords
+##### callback
 
-Type: `[string]`<br>
-Default: `[]`
+*Required*  
+Type: `function`
 
-Additional words to exclude from capitalization.
+A function to call once the DOM has loaded. If the DOM is already loaded, the callback will be called on the next tick.
 
-```js
-titleCase('Nothing to be afraid of?', {
-	excludedWords: ['be']
-});
-// 'Nothing to be Afraid Of?'
-```
-
-### useDefaultExcludedWords
-
-Type: `boolean`<br>
-Default: `true`
-
-Disable the usage of the default list of excluded words.
-
-```js
-titleCase('Nothing to be afraid of?', {
-	useDefaultExcludedWords: false
-});
-// 'Nothing To Be Afraid Of?'
-```
-
-### preserveWhitespace
-
-Type: `boolean`<br>
-Default: `false`
-
-Maintain extra whitespace between words. By default, all whitespace between words is collapsed to a single space.
-
-```js
-titleCase('Nothing  to be   afraid of?', {
-	preserveWhitespace: true
-});
-// 'Nothing  To Be   Afraid Of?'
-```
 
 ## License
 
-MIT © [Brad Dougherty](https://brad.is)
+MIT © [Ben Drucker](http://bendrucker.me)
--- index.js
+++ index.js
@@ -1,129 +1,17 @@
-const alwaysLowercase = [
-	'a',
-	'an',
-	'and',
-	'at',
-	'but',
-	'by',
-	'for',
-	'in',
-	'nor',
-	'of',
-	'on',
-	'or',
-	'so',
-	'the',
-	'to',
-	'up',
-	'yet',
-	'v',
-	'v.',
-	'vs',
-	'vs.'
-];
+'use strict'
 
-const containers = new Set(['(', '[', '{', '"', `'`, '_']);
+module.exports = ready
 
-const isEmail = /.+@.+\..+/;
-const isFilePath = /^(\/[\w.]+)+/;
-const isFileName = /^\w+\.\w{1,3}$/;
-const hasInternalCapital = /(?![‑–—-])[a-z]+[A-Z].*/;
-const hasHyphen = /[‑–—-]/g;
+function ready (callback) {
+  if (typeof document === 'undefined') {
+    throw new Error('document-ready only runs in the browser')
+  }
+  var state = document.readyState
+  if (state === 'complete' || state === 'interactive') {
+    return setTimeout(callback, 0)
+  }
 
-function isUrl(url) {
-	try {
-		const parsed = new URL(url);
-		return Boolean(parsed.hostname);
-	} catch {
-		return false;
-	}
-}
-
-function capitalize(string) {
-	if (string.length === 0) {
-		return string;
-	}
-
-	const letters = [...string];
-	const firstLetter = letters.shift();
-
-	if (containers.has(firstLetter)) {
-		return `${firstLetter}${capitalize(letters)}`;
-	}
-
-	return `${firstLetter.toUpperCase()}${letters.join('')}`;
-}
-
-export default function titleCase(
-	string = '',
-	{ excludedWords = [], useDefaultExcludedWords = true, preserveWhitespace = false } = {}
-) {
-	if (string.toUpperCase() === string) {
-		string = string.toLowerCase();
-	}
-
-	if (useDefaultExcludedWords) {
-		excludedWords.push(...alwaysLowercase);
-	}
-
-	const words = string.trim().split(/(\s+)/);
-
-	const capitalizedWords = words.map((word, index, array) => {
-		if (/\s+/.test(word)) {
-			return preserveWhitespace ? word : ' ';
-		}
-
-		if (
-			isEmail.test(word) ||
-			isUrl(word) ||
-			isFilePath.test(word) ||
-			isFileName.test(word) ||
-			hasInternalCapital.test(word)
-		) {
-			return word;
-		}
-
-		const hyphenMatch = word.match(hasHyphen);
-
-		if (hyphenMatch) {
-			const isMultiPart = hyphenMatch.length > 1;
-			const [hyphenCharacter] = hyphenMatch;
-
-			return word
-				.split(hyphenCharacter)
-				.map((subWord) => {
-					if (isMultiPart && excludedWords.includes(subWord.toLowerCase())) {
-						return subWord;
-					}
-
-					return capitalize(subWord);
-				})
-				.join(hyphenCharacter);
-		}
-
-		if (word.includes('/')) {
-			return word
-				.split('/')
-				.map((part) => capitalize(part))
-				.join('/');
-		}
-
-		const isFirstWord = index === 0;
-		const isLastWord = index === words.length - 1;
-		const previousWord = index > 1 ? array[index - 2] : '';
-		const startOfSubPhrase = index > 1 && previousWord.endsWith(':');
-
-		if (
-			!isFirstWord &&
-			!isLastWord &&
-			!startOfSubPhrase &&
-			excludedWords.includes(word.toLowerCase())
-		) {
-			return word.toLowerCase();
-		}
-
-		return capitalize(word);
-	});
-
-	return capitalizedWords.join('');
+  document.addEventListener('DOMContentLoaded', function onLoad () {
+    callback()
+  })
 }
