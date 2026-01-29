b34aae04bdac2ef4251c0619705860e1cc4bb22f
wu <wu@.com>
2026-01-29 11:36:05 +0800

    Logs-watson/https-pem

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-/**
- * Copyright 2013-2022 the PM2 project authors. All rights reserved.
- * Use of this source code is governed by a license that
- * can be found in the LICENSE file.
- */
+'use strict'
 
-process.env.PM2_PROGRAMMATIC = 'true';
+var path = require('path')
+var fs = require('fs')
 
-var API = require('./lib/API.js');
-
-module.exports = new API;
-module.exports.custom = API;
+exports.key = fs.readFileSync(path.join(__dirname, 'key.pem'))
+exports.cert = fs.readFileSync(path.join(__dirname, 'cert.pem'))
