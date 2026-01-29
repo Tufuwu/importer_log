e281f4e2c4e1654528aaa751bfc8bce0466447e7
wu <wu@.com>
2026-01-29 12:27:25 +0800

    Logs-cenkalti/pypi-notifier

--- src/docconvert/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import configuration
-from . import core
-from . import dict_utils
-from . import line_utils
-from . import parser
-from . import writer
-
-__version__ = "2.0.0"
-__version_info__ = tuple(int(i) for i in __version__.split(".") if i.isdigit())
