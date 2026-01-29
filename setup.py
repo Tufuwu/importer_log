d0e86ca6b471d9b0645b7d9d5ac76dbc896b0b15
wu <wu@.com>
2026-01-29 12:36:12 +0800

    Logs-davisnt/mopidy-alarmclock

--- setup.py
+++ setup.py
@@ -1,45 +1,51 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import re
 
-""" distribute- and pip-enabled setup.py """
+from setuptools import find_packages, setup
 
-import setuptools
-import sys
 
-import sparkdl
+def get_version(filename):
+    content = open(filename).read()
+    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", content))
+    return metadata['version']
 
-exclude_packages = ('tests', 'tests.*')
 
-if '--with-tests' in sys.argv:
-    index = sys.argv.index('--with-tests')
-    sys.argv.pop(index)
-    exclude_packages = ()
-
-setuptools.setup(
-    name='sparkdl',
-    version=sparkdl.__version__,
-    packages=setuptools.find_packages(exclude=exclude_packages),
-    url="https://github.com/databricks/spark-deep-learning",
-    author="Weichen Xu",
-    author_email="weichen.xu@databricks.com",
-    description="Deep Learning Pipelines for Apache Spark",
-    long_description="",
-    classifiers=[
-        "Intended Audience :: Education",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: Developers",
-        "Environment :: Console",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: Unix",
-        "Programming Language :: Python :: 3",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Software Development",
-    ],
-    platforms=["Linux"],
-    license="BSD",
-    keywords="spark deep learning horovod model distributed training",
-    install_requires=[],
-    extras_require={},
-    tests_require=["nose", "pytest"],
+setup(
+    name='Mopidy-AlarmClock',
+    version=get_version('mopidy_alarmclock/__init__.py'),
+    url='https://github.com/DavisNT/mopidy-alarmclock',
+    license='Apache License, Version 2.0',
+    author='Mathieu Xhonneux',
+    author_email='m.xhonneux@gmail.com',
+    maintainer='Davis Mosenkovs',
+    maintainer_email='python-apps@dm.id.lv',
+    description='A Mopidy extension for using it as an alarm clock.',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
     zip_safe=False,
+    include_package_data=True,
+    python_requires='>= 3.7',
+    install_requires=[
+        'setuptools',
+        'Mopidy >= 3.0',
+        'Pykka >= 1.1',
+        'monotonic >= 1.4',
+    ],
+    test_suite='nose.collector',
+    tests_require=[
+        'nose',
+        'mock >= 1.0',
+    ],
+    entry_points={
+        'mopidy.ext': [
+            'alarmclock = mopidy_alarmclock:Extension',
+        ],
+    },
+    classifiers=[
+        'Environment :: No Input/Output (Daemon)',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ],
 )
