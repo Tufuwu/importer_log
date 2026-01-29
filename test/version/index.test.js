21ad88070081b016cfc4118b6eee210e8e36c7c3
wu <wu@.com>
2026-01-29 09:27:52 +0800

    Logs-badeball/karma-jsdom-launcher

--- test/version/index.test.js
+++
@@ -1,7 +0,0 @@
-const pjson = require('../../package.json')
-const nudged = require('../../index')
-
-module.exports = (t) => {
-  t.equal(nudged.version, pjson.version, 'version match')
-  t.end()
-}
