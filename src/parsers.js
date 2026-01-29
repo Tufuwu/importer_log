73f9c82f63d5a7dff31176d0d383b3aea6f918c7
wu <wu@.com>
2026-01-29 11:06:47 +0800

    Logs-tcql/node-yesno

--- src/parsers.js
+++
@@ -1,8 +0,0 @@
-import createParser from './createParser';
-
-const camelToKebab = createParser(/[A-Z]/, match => `-${match.toLowerCase()}`);
-const snakeToKebab = createParser(/_/, () => '-');
-
-// disabled to allow named exports while only one named export exists
-// eslint-disable-next-line
-export { camelToKebab, snakeToKebab };
--- testSetup.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import * as matchers from 'jest-extended';
-
-expect.extend(matchers);
