377f6915c1566df6da4f2902d5bc1c58c8dfc1df
wu <wu@.com>
2026-01-29 09:04:38 +0800

    Logs-ace-diff/ace-diff

--- src/utils/preview-link.js
+++
@@ -1,9 +0,0 @@
-/**
- * Gets a link to preview a shortcut.
- * @param {string} shortcutID The ID of the shortcut to link to.
- * @returns {string} The preview link.
- */
-function getPreviewLink(shortcutID) {
-	return "https://showcuts.app/share/view/" + encodeURIComponent(shortcutID);
-}
-module.exports = getPreviewLink;
