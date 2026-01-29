2e29d2afa24c140a8aff9c145828d77ee1f09433
wu <wu@.com>
2026-01-29 12:51:34 +0800

    Logs-fetzerch/kasserver

--- setup.py
+++ setup.py
@@ -1,42 +1,26 @@
-from setuptools import setup, find_packages
-from codecs import open
-from os import path
+# kasserver - Manage domains hosted on All-Inkl.com through the KAS server API
+# Copyright (c) 2018 Christian Fetzer
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-# Get the long description from README.md
-with open('README.md', encoding='utf-8') as f:
-    long_description = f.read()
+"""Manage domains hosted on All-Inkl.com through the KAS server API"""
 
-setup(
-    name='cleanup',
-    version='1.0.3',
-    description='A simple command line utility that organises files in a '
-                'directory into subdirectories.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/faheel/cleanup',
-    author='Syed Faheel Ahmad',
-    author_email='faheel@live.in',
-    license='MIT',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Topic :: Utilities',
-    ],
-    keywords='cleanup file-organiser file-organisation file-management hue docopt',
-    packages=find_packages(exclude=['test']),
-    install_requires=['docopt', 'huepy'],
-    extras_require={
-        'dev': ['pylint'],
-    },
-    entry_points={
-        'console_scripts': [
-            'cleanup=cleanup.cleanup:main',
-        ],
-    },
-    project_urls={
-        'Bug Reports': 'https://github.com/faheel/cleanup/issues',
-        'Source': 'https://github.com/faheel/cleanup',
-    },
-)
+from setuptools import setup
+
+setup(setup_requires=["pbr"], pbr=True)
